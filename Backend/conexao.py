e1e0b23d6931c11006de2d1591e9a16ffdc4f6bf
Lucca <luccapessoamatos@gmail.com>
2019-05-03 18:23:50 -0300

    Commit
    Commit

---
+++ Backend/conexao.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May  3 16:56:05 2019
+
+@author: lpmatos
+"""
+
+import pyrebase
+
+config = {
+  "apiKey": "apiKey",
+  "authDomain": "gumplus-9d995.firebaseapp.com",
+  "databaseURL": "https://gumplus-9d995.firebaseio.com/",
+  "storageBucket": "gs://gumplus-9d995.appspot.com"
+}
